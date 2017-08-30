d048cf9360aa0f37f376a86bcec7dc0a5667f9f0
opensofias <sofias@radikalismus.com>
2017-08-30 15:20:35 +0200

    dynamic 'sprite', you are in space and can't hide
    also speed dependent on fps (bug)

--- walk.js
+++ walk.js
@@ -4,7 +4,7 @@ onload = ()=> {
 	d.body.appendChild (elem ({
 		tag: 'svg', svg: true,
 		attr: {
-			id: 'game', viewBox: '0 0 256 256',
+			id: 'game', viewBox: '-128 -128 256 256',
 			xmlns: 'http://www.w3.org/2000/svg'
 		},
 		content: [
@@ -12,7 +12,7 @@ onload = ()=> {
 				tag: 'polygon', svg: true,
 				attr: {
 					id: 'player', fill: '#f60',
-					points: '120,136 128,120 136,136',
+					points: polygonString (spriteMod.base),
 				}
 			}),
 			elem ({
@@ -22,14 +22,14 @@ onload = ()=> {
 			})
 		]
 	}))
-	setInterval (gameLoop, 1000 / 60)
+	requestAnimationFrame (gameLoop)
 }
 
 const makeTrees = ()=> 16 .map (num =>
 	elem ({
 		tag: 'circle', svg: true,
 		attr: {
-			cx: (num % 4 + .5) * 64 , cy: ((num >> 2) + .5) * 64, r: 8,
+			cx: (num % 4 -1.5) * 64 , cy: ((num >> 2) - 1.5) * 64, r: 8,
 			fill: '#480',
 		},
 	})
@@ -41,13 +41,15 @@ const polygonString = array =>
 	)
 
 const pressed = new Set ()
+let currentActions = []
 let	newKey = false
 let	position = [0, 0]
 let	angle = 0
 
 const actionMap = {
-	q: 'strafeleft', e: 'straferight',
-	w: 'up', s: 'down', a: 'left', d: 'right'
+	q: 'cw', e: 'ccw',
+	w: 'up', s: 'down',
+	a: 'left', d: 'right'
 }
 
 const keyListen = down => ({key}) => {
@@ -66,40 +68,64 @@ const gameLoop = ()=> {
 	if (newKey) {
 		showDebug ()
 
+		currentActions = [...pressed].map (key => actionMap [key] || key)
+
+		sel ('#player').setAttribute ('points',
+			polygonString (vecAdd (spriteMod.base,
+				...[...currentActions].map (action =>
+					spriteMod [action] || 8 .map (()=> 0)
+				)
+			))
+		)
 	} newKey = false
 
-	for (let key of pressed) {
-		if (actionMap [key]) key = actionMap [key]
-		const action = actions [key]
+	for (const actionWord of currentActions) {
+		const action = actions [actionWord]
 		action && action ()
 	}
 
 	pressed.size && sel ('#world').setAttribute (
 		'transform',
-		`rotate(${mod (angle * 360 / 256, 360)} 128 128) ` +
-		`translate (${position.join(' ')})`
+		`rotate(${mod (angle * 360 / 256, 360)}) ` +
+		`translate (${position.slice ().reverse ().join(' ')})`
 	)
+	
+	requestAnimationFrame (gameLoop)
+}
+
+const moveBy = (...vec) => ()=> {
+	position[0] +=
+		cos (angle * TAU / 256) * vec [0] -
+		sin (angle * TAU / 256) * vec [1]
+	position[1] += 
+		sin (angle * TAU / 256) * vec [0] +
+		cos (angle * TAU / 256) * vec [1]
 }
 
 const actions = {
-	left: ()=> angle ++,
-	right: ()=> angle --,
-	up: () => {
-		position[1] += cos (angle * TAU / 256)
-		position[0] += sin (angle * TAU / 256)
-	},
-	down: () => {
-		position[1] -= cos (angle * TAU / 256)
-		position[0] -= sin (angle * TAU / 256)
-	},
-	strafeleft: () => {
-		position[1] -= sin (angle * TAU / 256)
-		position[0] += cos (angle * TAU / 256)
-	},
-	straferight: () => {
-		position[1] += sin (angle * TAU / 256)
-		position[0] -= cos (angle * TAU / 256)
-	}
+	cw: ()=> angle ++,
+	ccw: ()=> angle --,
+	up: moveBy (1, 0),
+	down: moveBy (-1, 0),
+	left: moveBy (0, 1),
+	right: moveBy (0, -1)
+}
+
+const vecAdd = (...vecs) =>
+	vecs.reduce ((acc, cur) =>
+		acc.map ((comp, idx) =>
+			comp + cur [idx]
+		)
+	)
+
+const spriteMod = {
+	base: [-8, 0, 0, 8, 8, 0, 0, -8],
+	up: [0, 1, 0, -1, 0, 1, 0, -1],
+	down: [0, -1, 0, 1, 0, -1, 0, 1],
+	left: [-1, 0, 1, 0, -1, 0, 1, 0],
+	right: [1, 0, -1, 0, 1, 0, -1, 0],
+	cw: [0, -1, -1, 0, 0, 1, 1, 0],
+	ccw: [0, 1, 1, 0, 0, -1, -1, 0],
 }
 
 const showDebug = ()=> {
