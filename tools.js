f2efeddf9ee2a2c899ee5e0525eda88595ab1abd
sofia <12581975+opensofias@users.noreply.github.com>
2022-02-06 01:04:44 +0100

    unnessecary test for document

--- tools.js
+++ tools.js
@@ -2,28 +2,27 @@
 
 const [O, A, N, d] = [Object, Array, Number, document]
 
-const {elem, sel, selAll} = document ? {
-	elem ({tag = 'div', attr = {}, mixin = {}, content = [], svg = false}) {
-		const el = svg ?
-			d.createElementNS ('http://www.w3.org/2000/svg', tag) :
-			d.createElement (tag)
-		for (const name in attr)
-			typeof attr[name] == 'boolean' ?
-				attr[name] && el.setAttribute(name, name) :
-				el.setAttribute(name, attr[name])
+const elem = ({tag = 'div', attr = {}, mixin = {}, content = [], svg = false}) => {
+	const el = svg ?
+		d.createElementNS ('http://www.w3.org/2000/svg', tag) :
+		d.createElement (tag)
+	for (const name in attr)
+		typeof attr[name] == 'boolean' ?
+			attr[name] && el.setAttribute(name, name) :
+			el.setAttribute(name, attr[name])
 
-		void (type =>
-			['string', 'number'].includes (type) ?
-				el.innerText = content :
-			type == 'object' && [content].flat ()
-				.forEach(contEl => el.appendChild(contEl))
-		) (typeof content)
+	void (type =>
+		['string', 'number'].includes (type) ?
+			el.innerText = content :
+		type == 'object' && [content].flat ()
+			.forEach(contEl => el.appendChild(contEl))
+	) (typeof content)
 
-		return O.assign (el, mixin)
-	},
-	sel: selector => d.querySelector (selector),
-	selAll: selector => [...d.querySelectorAll(selector)]
-} : {}
+	return O.assign (el, mixin)
+}
+
+const sel = selector => d.querySelector (selector)
+const selAll = selector => [...d.querySelectorAll(selector)]
 
 const mod = (x, y) => ((x % y) + y) % y
 const {sin, cos, floor, ceil} = Math
